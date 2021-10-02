c5404bdf8576c2b23aa929cf8d9dcb7ff5b23b86
srpulga <edu.tads@outlook.com>
2021-10-02 10:23:24 -0300

    create database

---
+++ src/database/index.js
@@ -0,0 +1,22 @@
+const { Client } = require('pg');
+
+// dados de conexão do banco
+
+const client = new Client({
+  host: 'localhost',
+  port: 5432,
+  user: 'root',
+  password: 'root',
+  database: 'stocksystem',
+});
+
+client.connect();
+console.log('Banco Conectado!');
+
+exports.query = async (query, values) => {
+  const { rows } = await client.query(query, values);
+  return rows;
+};
+
+// Query('SELECT * FROM contacts').then((result) => console.log(result));
+// seleciona todos os dados da tabela contatos
---
+++ src/database/schema.sql
@@ -0,0 +1,32 @@
+-- Utilizando Postgres (docker)
+
+CREATE DATABASE stocksystem;
+
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp"; -- Colocar extensão do uuid
+
+-- Cria tabela de ingredientes
+CREATE TABLE IF NOT EXISTS ingredient (
+  id UUID NOT NULL UNIQUE DEFAULT uuid_generate_v4(),
+  name VARCHAR NOT NULL,
+  unitmeasure VARCHAR NOT NULL,
+  unitprice VARCHAR NOT NULL
+);
+
+-- Cria tablea de produtos relacionado ao id dos ingredientes
+CREATE TABLE IF NOT EXISTS product (
+  id UUID NOT NULL UNIQUE DEFAULT uuid_generate_v4(),
+  name VARCHAR NOT NULL,
+  imagename VARCHAR NOT NULL,
+  image BYTEA NOT NULL,
+  ingredient_id UUID,
+  FOREIGN KEY(ingredient_id) REFERENCES ingredient(id)
+);
+
+-- Cria tabela dos usuarios
+
+CREATE TABLE IF NOT EXISTS users (
+  id UUID NOT NULL UNIQUE DEFAULT uuid_generate_v4(),
+  name VARCHAR NOT NULL,
+  email VARCHAR NOT NULL,
+  senha VARCHAR NOT NULL
+);
